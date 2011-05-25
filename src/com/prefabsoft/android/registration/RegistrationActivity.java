ad7dd8efaab39583d20317c87f4282d0c3e76cf3
Jochen <Jochen@prefabsoft.com>
2011-05-25 03:30:25 +0200

    init

---
+++ src/com/prefabsoft/android/registration/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.prefabsoft.android.registration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
